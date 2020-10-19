2be3dc44b5ff7da6f3ad2fd759940dd594f140a1
karwoant <karwoant@localhost.localdomain>
2020-10-19 15:30:14 +0200

    tablica

--- Sudoku/src/main/java/SudokuBoard.java
+++ Sudoku/src/main/java/SudokuBoard.java
@@ -1,3 +1,41 @@
 public class SudokuBoard {
     private int board[][] = new int[9][9];
+
+    public void fillBoard(){
+
+    }
+
+    private boolean checkColumn(int c){
+        int value = 0;
+        for (int i = 0 ; i < 9 ; i++) {
+            value += board[i][c];
+        }
+        if(value==9){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkRow(int r){
+        int value = 0;
+        for (int i = 0 ; i < 9 ; i++) {
+            value += board[r][i];
+        }
+        if(value==9){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkbox(int c, int r){
+        int value = 0;
+        for (int i = 0 ; i < 9 ; i++) {
+            value += board[r][i];
+        }
+        if(value==9){
+            return true;
+        }
+        return false;
+    }
 }
+
