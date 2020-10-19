106cb8cf008651d4dbc1042a1972913449680022
229879 Gebel Michał <229879@edu.p.lodz.pl>
2020-10-19 19:20:21 +0200

    losowa katasrofa powraca

--- Sudoku/src/main/java/SudokuBoard.java
+++ Sudoku/src/main/java/SudokuBoard.java
@@ -1,57 +1,49 @@
+import java.util.Random;
+
 public class SudokuBoard {
-    private final int[][] board = new int[9][9];
+    private int board[][] = new int[9][9];
 
     public void fillBoard() {
-        int[][] oldBoard = new int[9][9];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                oldBoard[i][j] = board[i][j];
-            }
-        }
+
         int k = 1;
 
         for (int i = 0; i < 9; i++) {
 
             for (int j = 0; j < 9; j++) {
 
-                if (oldBoard[i][j] == 0) {
+                //sprawdź czy dane pole nalezy do poczatkowo wypelnionych
 
-                    while (true) {
+                while (true) {
 
-                        if (k > 9) {   // jeżeli do danego pola nie pasuje żadna liczba cofamy się do poprzedniego pola zwiększając jego wartość o jeden
-                            board[i][j] = 0;
+                    if (k > 9) {
+                        board[i][j] = 0;
 
-                            int[] params = getLastCell(i, j);
-                            k = board[params[0]][params[1]] + 1;
-                            i = params[0];
-                            j = params[1] - 1;
-
-                            break;
+                        if (j == 0) {
+                            k = board[i - 1][8] + 1;
+                            i--;
+                            j = 7;
+                        } else {
+                            k = board[i][j - 1] + 1;
+                            j = j - 2;
                         }
 
-                        board[i][j] = k;
-                        if (viablityTest(i, j)) {
-                            k = 1;
-                            break;
-                        }
-                        k++;
+                        break;
                     }
+
+                    board[i][j] = k;
+                    if (viablityTest(i, j)) {
+
+                        k = 1;
+                        break;
+                    }
+                    k++;
                 }
             }
         }
     }
 
-    private int[] getLastCell(int i, int j) {
-        int[] result = new int[2];
-        if (j == 0) {
-            result[0] = i - 1;
-            result[1] = 8;
-        } else {
-            result[0] = i;
-            result[1] = j - 1;
-        }
-        return result;
-    }
+
+    //  public getNextCell
 
     private boolean viablityTest(int row, int column) {
 
@@ -102,10 +94,27 @@ public class SudokuBoard {
                 System.out.print(board[i][j] + " ");
             }
             System.out.print("|");
-            System.out.println();
+            System.out.println("");
         }
         System.out.println("-------------------------");
     }
 
+    private int generateSudokuFigures() {
+        Random rand = new Random();
+        int figure = 0;
+        figure = rand.nextInt(9) + 1;
+        return figure;
+    }
+
+    public void randomFillBoard(){
+        int column = 0;
+        for(int i = 0; i < 10; i++){
+            column = generateSudokuFigures();
+            do {
+                board[i][column] = generateSudokuFigures();
+            } while(!viablityTest(i, column));
+        }
+    }
 }
+
 
