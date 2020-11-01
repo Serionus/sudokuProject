fbddefbbde32d2dc040732f08fa85a47441742a5
karwowskiantoni <karwowski.spam@o2.pl>
2020-11-01 19:48:27 +0100

    ostatnie poprawki

--- Sudoku/src/main/java/Sudoku.java
+++ Sudoku/src/main/java/Sudoku.java
@@ -1,6 +1,11 @@
 public class Sudoku {
     public static void main(String[] args) {
 
+        //metoda "main" jest zbędna i nieuwzględniona w zadaniu, jednak
+        //zostawiliśmy ją w celu prostego wyświetlania wyników pracy,
+        //jest to niekiedy przydatne do zwizualizowania problemów
+        //i niemniej satysfakcjonujące
+
         BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
         SudokuBoard board = new SudokuBoard(solver);
 
