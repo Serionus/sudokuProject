dda7c1312d52b02b577de573e7d76626f6aae150
229879 Gebel Michał <229879@edu.p.lodz.pl>
2020-12-18 13:59:42 +0100

    zadanie 8 Scene design

--- SudokuGame/View/src/main/java/org/example/PrimaryController.java
+++ SudokuGame/View/src/main/java/org/example/PrimaryController.java
@@ -1,14 +1,28 @@
 package org.example;
 
 import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
 
 public class PrimaryController {
+    ObservableList<String> difficulties = FXCollections.observableArrayList("Hard", "Mega Hard", "Super Mega Ultra Hard");
+    private int chosenDifficulty = 1;  // 1 - hard, 2 - mega hard, 3 - super mega ultra hard
+
+    @FXML
+    private ChoiceBox levelChooser;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
 
+    @FXML
+    private void initialize(){
+        levelChooser.setValue("Hard");
+        levelChooser.setItems(difficulties);
     }
 
     public static void main(String[] args) {
--- SudokuGame/View/src/main/java/org/example/SecondaryController.java
+++ SudokuGame/View/src/main/java/org/example/SecondaryController.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
 
