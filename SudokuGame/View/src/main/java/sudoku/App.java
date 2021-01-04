20dc5331b7b37a5436035c876ad9779205ce8055
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-01-04 13:32:59 +0100

    zadanie 10 save/load

--- SudokuGame/View/src/main/java/sudoku/App.java
+++ SudokuGame/View/src/main/java/sudoku/App.java
@@ -3,6 +3,7 @@ package sudoku;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
--- SudokuGame/View/src/main/java/sudoku/SecondaryController.java
+++ SudokuGame/View/src/main/java/sudoku/SecondaryController.java
@@ -1,10 +1,12 @@
 package sudoku;
 
+import java.io.File;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
