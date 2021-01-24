82ddd36dedd61bf6627c558f85adef2ba78ba019
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-01-24 23:19:40 +0100

    zadanie 12 struggling

---
+++ SudokuGame/Model/src/main/java/sudoku/JPASudokuBoardDao.java
@@ -0,0 +1,47 @@
+package sudoku;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class JPASudokuBoardDao implements Dao<SudokuBoard>{
+    String fileName;
+    ResourceBundle bundle;
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("SudokuBoard");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    SudokuBoardRepository repository = new SudokuBoardRepository(entityManager);
+
+    public JPASudokuBoardDao(String fileName) {
+        this.fileName = fileName;
+        bundle = ResourceBundle.getBundle("SudokuBundle", Locale.getDefault());
+    }
+
+    public JPASudokuBoardDao(String fileName, ResourceBundle bundle) {
+        this.fileName = fileName;
+        this.bundle = bundle;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public SudokuBoard read() {
+        return null;
+//        return repository.findByName(fileName);
+    }
+
+    @Override
+    public void write(SudokuBoard board) {
+            repository.save(board, fileName);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+}
